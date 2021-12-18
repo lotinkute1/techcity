2b85ac599d37145aaafe41effda5ac91ca1ebc23
tinnguyen14 <tinshb@gmail.com>
2021-12-18 14:20:57 +0700

    begin cua tin

---
+++ src/features/ShowAllProduct/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './style.css'
+class ShowAllProduct extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ShowAllProduct;
