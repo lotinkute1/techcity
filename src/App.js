7becc433582ed6a2d52f5ed9824245a5087e65fb
Huy X <lotinkute1@gmail.com>
2021-12-21 17:17:06 +0700

    chuyen info product vao project

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from "./components/Header2/index";
 import Main from './components/Main/Main';
-
+import ProductInfo from './features/ProductInfo/ProductInfo';
 function App() {
   
 
   return (
     <div className="wrapper">
       <Header/>
-      <Main/>
+      {/* <Main/> */}
+      <ProductInfo/>
       <Footer/>
     </div>
 
