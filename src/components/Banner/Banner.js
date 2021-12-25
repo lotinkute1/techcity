fd081574aaa81478ee56b09429d44923724e0dbe
Huy X <lotinkute1@gmail.com>
2021-12-25 16:48:55 +0700

    huy dep trai day du lieu len banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,34 +4,32 @@ import slide2 from "../../assets/images/slides/slide2.webp";
 import slide3 from "../../assets/images/slides/slide3.webp";
 import slide4 from "../../assets/images/slides/slide4.webp";
 import GetDiscountsData from "../../api/GetDiscountsData";
+import Slider from "react-slick";
+import {sliderBannerSetting} from "../settings/slickSetting"
 export default function Banner() {
-//  console.log(GetDiscountsData())
- const discountsData =GetDiscountsData();
-//  console.log(discountsData[0].discount_img);
+  //  console.log(GetDiscountsData())
+  const discountsData = GetDiscountsData();
+  //  console.log(discountsData[0].discount_img);
+  const renderBannerImg = () =>
+    discountsData.map((item) => {
+      return (
+        <div key={item.id} className="carousel-cell">
+          <a href="/#">
+            <img src={item.discount_img} alt=""></img>
+          </a>
+        </div>
+      );
+    });
   return (
     <section className="section banner">
-      <div className="carousel" data-flickity='{"autoPlay": 5000}'>
-        <div className="carousel-cell">
-          <a href="/#">
-            <img src={slide2} alt=""></img>
-          </a>
-        </div>
-        <div className="carousel-cell">
-          <a href="/#">
-            <img src={slide2} alt=""></img>
-          </a>
-        </div>
-        <div className="carousel-cell">
-          <a href="/#">
-            <img src={slide3} alt=""></img>
-          </a>
-        </div>
-        <div className="carousel-cell">
-          <a href="/#">
-            <img src={slide4} alt=""></img>
-          </a>
-        </div>
-      </div>
+      {/* <div className="carousel" data-flickity='{"autoPlay": 5000}'>
+    	{renderBannerImg()}
+        
+      </div> */}
+		<Slider {...sliderBannerSetting}>
+			{renderBannerImg()}
+		</Slider>
+
     </section>
   );
 }
---
+++ src/components/settings/slickSetting.js
@@ -0,0 +1,20 @@
+export const sliderSetting1 = {
+  dots: false,
+  infinite: false,
+  rows: 1,
+  autoplay: true,
+  speed: 500,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplaySpeed: 2000,
+  adaptiveHeight: false,
+  lazyLoad: true,
+};
+
+export const sliderBannerSetting = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
