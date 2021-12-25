c947fbe09d2432eeb9534b907fbb53c811992730
tinnguyen14 <tinshb@gmail.com>
2021-12-25 10:02:51 +0700

    commit

---
+++ src/features/ShowAllProduct/index.jsx
@@ -0,0 +1,264 @@
+import React, { Component } from 'react';
+import './style.css'
+class ShowAllProduct extends Component {
+    render() {
+        return (
+                  <div className="show-all">
+  <div className="container allproduct">
+    <div className="row product-filter py-3">
+      <h5><a href style={{color: 'rgba(128, 128, 128, 0.637) !important'}}>TRANG CHỦ</a> / CỬA HÀNG</h5>
+      <div className="filter">
+        <span className="mr-1">Hiển thị 1–12 trong 28 kết quả</span>
+        <select name="product" id="product">
+          <option value="volvo">Thứ tự mặc định</option>
+          <option value="saab">Thứ tự theo mức độ phổ biến</option>
+          <option value="mercedes">Mới nhất</option>
+          <option value="audi">Thứ tự theo điểm đánh giá</option>
+        </select>
+      </div>
+    </div>
+    <div className="row all-product">
+      <div className="col-md-3 shop-bar">
+        <div className="col-sm-12" style={{color: 'red', fontWeight: 700}}>
+          Danh mục sản phẩm
+        </div>
+        <div className="col-sm-12 option bg">
+          <a href>Điện thoại</a>
+        </div>
+        <div className="col-sm-12 option">
+          <a href>Máy tính</a>
+        </div>
+        <div className="col-sm-12 option bg">
+          <a href>Đồng hồ</a>
+        </div>
+        <div className="col-sm-12 option">
+          <a href>Âm thanh</a>
+        </div> 
+        <div className="col-sm-12 option bg">
+          <a href>Smart home</a>
+        </div>
+        <div className="col-sm-12 option">
+          <a href>Đang giảm giá</a>
+        </div>
+      </div>
+      <div className="col-md-9 all-item">
+        <div className="row">
+          <div className="col-sm-3 carousel-items">
+            <div className="col-sm-12">
+              <a href className="carousel-itemimage"><img src="https://images.fpt.shop/unsafe/fit-in/585x390/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2021/8/17/637647989512049638_asus-vivobook-a515-bac-1.jpg" alt="" /></a>
+              <div className="carousel-itembot-wrapper">
+                <a href className="carousel-itemtitle">
+                  <h3 className="test-wrap">LAPTOP</h3>
+                </a>
+                <div className="carousel-itemraiting">
+                  <div className="raiting-starts">
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                  </div>
+                </div>
+                <div className="carousel-itemprice">
+                  <span>32.900.000đ</span>
+                  <span><i className="fas fa-truck" /></span>
+                </div>
+                <div className="carousel-itemadd-to-cart">
+                  <div className="btn btn-buy">Thêm vào giỏ</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-3 carousel-items">
+            <div className="col-sm-12">
+              <a href className="carousel-itemimage"><img src="https://images.fpt.shop/unsafe/fit-in/585x390/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2021/8/17/637647989512049638_asus-vivobook-a515-bac-1.jpg" alt="" /></a>
+              <div className="carousel-itembot-wrapper">
+                <a href className="carousel-itemtitle">
+                  <h3 className="test-wrap">LAPTOP</h3>
+                </a>
+                <div className="carousel-itemraiting">
+                  <div className="raiting-starts">
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                  </div>
+                </div>
+                <div className="carousel-itemprice">
+                  <span>32.900.000đ</span>
+                  <span><i className="fas fa-truck" /></span>
+                </div>
+                <div className="carousel-itemadd-to-cart">
+                  <div className="btn btn-buy">Thêm vào giỏ</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-3 carousel-items">
+            <div className="col-sm-12">
+              <a href className="carousel-itemimage"><img src="https://images.fpt.shop/unsafe/fit-in/585x390/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2021/8/17/637647989512049638_asus-vivobook-a515-bac-1.jpg" alt="" /></a>
+              <div className="carousel-itembot-wrapper">
+                <a href className="carousel-itemtitle">
+                  <h3 className="test-wrap">LAPTOP</h3>
+                </a>
+                <div className="carousel-itemraiting">
+                  <div className="raiting-starts">
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                  </div>
+                </div>
+                <div className="carousel-itemprice">
+                  <span>32.900.000đ</span>
+                  <span><i className="fas fa-truck" /></span>
+                </div>
+                <div className="carousel-itemadd-to-cart">
+                  <div className="btn btn-buy">Thêm vào giỏ</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-3 carousel-items">
+            <div className="col-sm-12">
+              <a href className="carousel-itemimage"><img src="https://images.fpt.shop/unsafe/fit-in/585x390/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2021/8/17/637647989512049638_asus-vivobook-a515-bac-1.jpg" alt="" /></a>
+              <div className="carousel-itembot-wrapper">
+                <a href className="carousel-itemtitle">
+                  <h3 className="test-wrap">LAPTOP</h3>
+                </a>
+                <div className="carousel-itemraiting">
+                  <div className="raiting-starts">
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                  </div>
+                </div>
+                <div className="carousel-itemprice">
+                  <span>32.900.000đ</span>
+                  <span><i className="fas fa-truck" /></span>
+                </div>
+                <div className="carousel-itemadd-to-cart">
+                  <div className="btn btn-buy">Thêm vào giỏ</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-3 carousel-items">
+            <div className="col-sm-12">
+              <a href className="carousel-itemimage"><img src="https://images.fpt.shop/unsafe/fit-in/585x390/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2021/8/17/637647989512049638_asus-vivobook-a515-bac-1.jpg" alt="" /></a>
+              <div className="carousel-itembot-wrapper">
+                <a href className="carousel-itemtitle">
+                  <h3 className="test-wrap">LAPTOP</h3>
+                </a>
+                <div className="carousel-itemraiting">
+                  <div className="raiting-starts">
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                  </div>
+                </div>
+                <div className="carousel-itemprice">
+                  <span>32.900.000đ</span>
+                  <span><i className="fas fa-truck" /></span>
+                </div>
+                <div className="carousel-itemadd-to-cart">
+                  <div className="btn btn-buy">Thêm vào giỏ</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-3 carousel-items">
+            <div className="col-sm-12">
+              <a href className="carousel-itemimage"><img src="https://images.fpt.shop/unsafe/fit-in/585x390/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2021/8/17/637647989512049638_asus-vivobook-a515-bac-1.jpg" alt="" /></a>
+              <div className="carousel-itembot-wrapper">
+                <a href className="carousel-itemtitle">
+                  <h3 className="test-wrap">LAPTOP</h3>
+                </a>
+                <div className="carousel-itemraiting">
+                  <div className="raiting-starts">
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                  </div>
+                </div>
+                <div className="carousel-itemprice">
+                  <span>32.900.000đ</span>
+                  <span><i className="fas fa-truck" /></span>
+                </div>
+                <div className="carousel-itemadd-to-cart">
+                  <div className="btn btn-buy">Thêm vào giỏ</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-3 carousel-items">
+            <div className="col-sm-12">
+              <a href className="carousel-itemimage"><img src="https://images.fpt.shop/unsafe/fit-in/585x390/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2021/8/17/637647989512049638_asus-vivobook-a515-bac-1.jpg" alt="" /></a>
+              <div className="carousel-itembot-wrapper">
+                <a href className="carousel-itemtitle">
+                  <h3 className="test-wrap">LAPTOP</h3>
+                </a>
+                <div className="carousel-itemraiting">
+                  <div className="raiting-starts">
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                  </div>
+                </div>
+                <div className="carousel-itemprice">
+                  <span>32.900.000đ</span>
+                  <span><i className="fas fa-truck" /></span>
+                </div>
+                <div className="carousel-itemadd-to-cart">
+                  <div className="btn btn-buy">Thêm vào giỏ</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-3 carousel-items">
+            <div className="col-sm-12">
+              <a href className="carousel-itemimage"><img src="https://images.fpt.shop/unsafe/fit-in/585x390/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2021/8/17/637647989512049638_asus-vivobook-a515-bac-1.jpg" alt="" /></a>
+              <div className="carousel-itembot-wrapper">
+                <a href className="carousel-itemtitle">
+                  <h3 className="test-wrap">LAPTOP</h3>
+                </a>
+                <div className="carousel-itemraiting">
+                  <div className="raiting-starts">
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                    <i className="fas fa-star" />
+                  </div>
+                </div>
+                <div className="carousel-itemprice">
+                  <span>32.900.000đ</span>
+                  <span><i className="fas fa-truck" /></span>
+                </div>
+                <div className="carousel-itemadd-to-cart">
+                  <div className="btn btn-buy">Thêm vào giỏ</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+        );
+    }
+}
+
+export default ShowAllProduct;
